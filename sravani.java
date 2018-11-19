bee53192358dc912d4e4c8eed5b3c9d449c16a3e
suryadattaharish <suryavaddadi1@gmail.com>
2018-11-19 22:32:49 +0530

    updated pranamya.java

--- sravani.java
+++ sravani.java
@@ -3,8 +3,12 @@ she is my sister
 
 about to complete her studies
 
-soon she will get a good job
+soon she will get a good job in one of the best MNC
 
 hope she will be happy forever
 
 god bless
+
+thank you
+
+
